9fb5f1d88eb0f96f1acce012267775a24656c684
Marcelo Leiva <leivaskt@gmail.com>
2023-05-29 14:09:22 -0300

    ejer 2

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Titulos from "./components/Titulos";
+function App() {
+ 
+
+  return (
+    <>
+      <Titulos nuevoTitulo= 'Hola Amigos' estado={true}/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Titulos.jsx
@@ -0,0 +1,12 @@
+const Titulos = ({nuevoTitulo}) => {
+
+
+  return (
+    <section className="text-center my-5">
+      <h1 >Hola Mundo</h1>
+      <h2 >{nuevoTitulo}</h2>
+    </section>
+  );
+};
+
+export default Titulos;
